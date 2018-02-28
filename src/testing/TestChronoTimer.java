dddf55914b665641f80eafcc617df2ea8817034f
Alex Mitchell <amrykid@gmail.com>
2018-02-27 21:29:54 -0600

    Beginning of "testChrono1"

--- src/testing/TestChronoTimer.java
+++ src/testing/TestChronoTimer.java
@@ -1,15 +1,45 @@
 package testing;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import core.Chronotimer;
 
 public class TestChronoTimer {
-	
-	Chronotimer chronotimer = new Chronotimer();
-	
 	@Test
-	public void test1(){
+	public void testChrono1(){
+		//unit test version of chrono1.txt
+		Chronotimer chronotimer = new Chronotimer();
 		
+		//"TIME 12:00:01.0"
+		chronotimer.setTime(12, 0, 1);
+		assertEquals("12:0:1.0", chronotimer.getTime());
+		
+		//"POWER"
+		assertEquals(false, chronotimer.getIsPower());
+		chronotimer.togglePower();
+		assertEquals(true, chronotimer.getIsPower());
+		
+		//"CONN GATE 1"
+		chronotimer.setConnect(1, "GATE");
+		assertEquals("GATE", chronotimer.getSensorType(1));
+		
+		chronotimer.setConnect(2, "EYE");
+		assertEquals("EYE", chronotimer.getSensorType(2));
+		
+		chronotimer.setConnect(3, "GATE");
+		assertEquals("GATE", chronotimer.getSensorType(3));
+		
+		chronotimer.setConnect(2, "EYE");
+		assertEquals("EYE", chronotimer.getSensorType(2));
+		
+		//"TOG 1"
+		assertEquals(false, chronotimer.getChannel(1));
+		chronotimer.toggleChannel(1);
+		assertEquals(true, chronotimer.getChannel(1));
+		
+		assertEquals(false, chronotimer.getChannel(2));
+		chronotimer.toggleChannel(2);
+		assertEquals(true, chronotimer.getChannel(2));
 	}
 
 }
