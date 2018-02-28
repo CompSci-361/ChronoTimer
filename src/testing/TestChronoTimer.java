c78ae482d9124c95c15aea3b47e28551f0cf9036
Alex Mitchell <amrykid@gmail.com>
2018-02-27 23:10:24 -0600

    Updated tests to take advantage of the new function names.

--- src/testing/TestChronoTimer.java
+++ src/testing/TestChronoTimer.java
@@ -2,7 +2,7 @@ package testing;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import core.Chronotimer;
+import core.*;
 
 public class TestChronoTimer {
 	@Test
@@ -12,41 +12,41 @@ public class TestChronoTimer {
 		
 		//"TIME 12:00:01.0"
 		chronotimer.setTime(12, 0, 1);
-		assertEquals("12:0:1.0", chronotimer.getTime());
+		assertEquals("12:0:1.0", chronotimer.getTime()); //getTime may sometimes add .01 to the end for some reason.
 		
 		//"POWER"
-		assertEquals(false, chronotimer.getIsPower());
+		assertEquals(false, chronotimer.getIsPoweredOn());
 		chronotimer.togglePower();
-		assertEquals(true, chronotimer.getIsPower());
+		assertEquals(true, chronotimer.getIsPoweredOn());
 		
 		//"CONN GATE 1"
-		chronotimer.setConnect(1, "GATE");
-		assertEquals("GATE", chronotimer.getSensorType(1));
+		chronotimer.setConnect(1, SensorType.GATE);
+		assertEquals(SensorType.GATE, chronotimer.getSensorType(1));
 		
-		chronotimer.setConnect(2, "EYE");
-		assertEquals("EYE", chronotimer.getSensorType(2));
+		chronotimer.setConnect(2, SensorType.EYE);
+		assertEquals(SensorType.EYE, chronotimer.getSensorType(2));
 		
-		chronotimer.setConnect(3, "GATE");
-		assertEquals("GATE", chronotimer.getSensorType(3));
+		chronotimer.setConnect(3, SensorType.GATE);
+		assertEquals(SensorType.GATE, chronotimer.getSensorType(3));
 		
-		chronotimer.setConnect(2, "EYE");
-		assertEquals("EYE", chronotimer.getSensorType(2));
+		chronotimer.setConnect(4, SensorType.EYE);
+		assertEquals(SensorType.EYE, chronotimer.getSensorType(4));
 		
 		//"TOG 1"
-		assertEquals(false, chronotimer.getChannel(1));
+		assertEquals(false, chronotimer.getChannelIsEnabled(1));
 		chronotimer.toggleChannel(1);
-		assertEquals(true, chronotimer.getChannel(1));
+		assertEquals(true, chronotimer.getChannelIsEnabled(1));
 		
-		assertEquals(false, chronotimer.getChannel(2));
+		assertEquals(false, chronotimer.getChannelIsEnabled(2));
 		chronotimer.toggleChannel(2);
-		assertEquals(true, chronotimer.getChannel(2));
+		assertEquals(true, chronotimer.getChannelIsEnabled(2));
 		
 		//todo: handle the "trig 1" * commands
 		
 		//"POWER"
-		assertEquals(true, chronotimer.getIsPower());
+		assertEquals(true, chronotimer.getIsPoweredOn());
 		chronotimer.togglePower();
-		assertEquals(false, chronotimer.getIsPower());
+		assertEquals(false, chronotimer.getIsPoweredOn());
 		
 		//"EXIT"
 	}
@@ -57,19 +57,19 @@ public class TestChronoTimer {
 		Chronotimer chronotimer = new Chronotimer();
 		
 		//"POWER"
-		assertEquals(false, chronotimer.getIsPower());
+		assertEquals(false, chronotimer.getIsPoweredOn());
 		chronotimer.togglePower();
-		assertEquals(true, chronotimer.getIsPower());
+		assertEquals(true, chronotimer.getIsPoweredOn());
 		
 		//"POWER"
-		assertEquals(true, chronotimer.getIsPower());
+		assertEquals(true, chronotimer.getIsPoweredOn());
 		chronotimer.togglePower();
-		assertEquals(false, chronotimer.getIsPower());
+		assertEquals(false, chronotimer.getIsPoweredOn());
 		
 		//"POWER"
-		assertEquals(false, chronotimer.getIsPower());
+		assertEquals(false, chronotimer.getIsPoweredOn());
 		chronotimer.togglePower();
-		assertEquals(true, chronotimer.getIsPower());
+		assertEquals(true, chronotimer.getIsPoweredOn());
 
 		
 	}
