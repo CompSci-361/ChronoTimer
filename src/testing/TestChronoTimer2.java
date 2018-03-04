019dba27c376bb747f99e9424d47b3cd621f0b7d
cjscholl <catelynjscholl@gmail.com>
2018-03-04 14:03:25 -0600

    More extensive testing
    Implemented a test with more racers and also no racers. Passes both tests. Also, swap is only for IND.

--- src/testing/TestChronoTimer2.java
+++ src/testing/TestChronoTimer2.java
@@ -31,4 +31,43 @@ public class TestChronoTimer2 {
 		
 		assertEquals(11, chronotimer.getCurrentRun().getCurrentRunningRacer().getBibNumber());
 	}
+	
+	@Test
+	public void testSwap2(){
+		chronotimer.togglePower();
+		chronotimer.setRaceType(RaceType.valueOf("IND"));
+		chronotimer.newRun();
+				
+		chronotimer.addRacer(10);
+		chronotimer.addRacer(11);
+		chronotimer.addRacer(12);
+		chronotimer.addRacer(13);
+		
+		chronotimer.toggleChannel(1);
+		chronotimer.toggleChannel(2);
+		assertTrue(chronotimer.getChannelIsEnabled(1));
+		assertTrue(chronotimer.getChannelIsEnabled(2));
+		
+		chronotimer.triggerChannel(1);
+		chronotimer.triggerChannel(1);
+		chronotimer.triggerChannel(1);
+		chronotimer.triggerChannel(1);
+		
+		chronotimer.swap();
+		
+		assertEquals(11, chronotimer.getCurrentRun().getCurrentRunningRacer().getBibNumber());
+		
+		chronotimer.triggerChannel(2);
+		chronotimer.triggerChannel(2);
+		
+		chronotimer.swap();
+		
+		assertEquals(13, chronotimer.getCurrentRun().getCurrentRunningRacer().getBibNumber());
+		
+		chronotimer.triggerChannel(2);
+		chronotimer.triggerChannel(2);
+		
+		assertEquals(null, chronotimer.getCurrentRun().getCurrentRunningRacer());
+
+	}
 }
