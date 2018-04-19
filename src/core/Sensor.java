9f06cf3be0e81239489b1ffaeedc05fadcae0436
Alex Mitchell <Amrykid@gmail.com>
2018-04-18 19:45:15 -0700

    sensor threads.

--- src/core/Sensor.java
+++ src/core/Sensor.java
@@ -1,12 +1,30 @@
 package core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 public class Sensor {
 	private SensorType sensorType;
-	private int channelNumber; 
+	private int channelNumber;
+	private Thread sensorThread = null;
+	private volatile boolean isListening = false;
+	private ArrayList<ActionListener> sensorListeners = null;
 	Sensor(SensorType type, int channelNumber) {
 		sensorType = type;
 		this.channelNumber = channelNumber;
+		sensorListeners = new ArrayList<ActionListener>();
+		isListening = true;
+		sensorThread = new Thread(new SensorThread());
+		sensorThread.start();
 	}
+	
+	public void addSensorFiredActionListener(ActionListener listener) {
+		if (!sensorListeners.contains(listener)) {
+			sensorListeners.add(listener);
+		}
+	}
+	
 	public int getChannelNumber() {
 		return channelNumber;
 	}
@@ -15,7 +33,27 @@ public class Sensor {
 		return sensorType;
 	}
 	public void close() {
+		sensorListeners.clear();
 		
-		
+		isListening = false;
+	}
+	
+	private class SensorThread implements Runnable {
+		@Override
+		public void run() {
+			while(isListening) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					//fire our sensor
+					
+					for(ActionListener listener : sensorListeners) {
+						listener.actionPerformed(new ActionEvent(Sensor.this, 1, 
+								Sensor.this.getChannelNumber() + ":" + Sensor.this.getSensorType().toString()));
+					}
+				}
+			}
+			
+		}
 	}
 }
