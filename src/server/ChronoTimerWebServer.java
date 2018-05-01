f9e1d31e765889a08eb9bd0d2c03dca37b81f186
Amrykid <amrykid@gmail.com>
2018-05-01 10:42:49 -0500

    Create a separate server for chronotimers to connect to. #61

--- src/server/ChronoTimerWebServer.java
+++ src/server/ChronoTimerWebServer.java
@@ -1,9 +1,11 @@
 package server;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -12,23 +14,28 @@ import core.Chronotimer;
 
 public class ChronoTimerWebServer {
 	private static boolean isInitialized = false;
-	private static Chronotimer serverTimer = null;
 	private static HttpServer httpServer = null;
+	private static HttpServer chronoServer = null;
 	
-	public static void initialize(Chronotimer chronoTimer) throws IOException {
+	public static void main(String[] args) throws IOException {
+		System.out.print("Initializing server... ");
+		initialize();
+		System.out.println("Done");
+	}
+	
+	public static void initialize() throws IOException {
 		if (isInitialized) return;
-		if (chronoTimer == null) throw new IllegalArgumentException("chronoTimer");
-		serverTimer = chronoTimer;
 		
+		//create a http server for browsers.
 		httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
-		
-        // create a context to get the request to display the results
 		httpServer.createContext("/", new IndexHandler());
-        //server.createContext("/style.css", new StyleHandler());
-
-		httpServer.setExecutor(null); // creates a default executor
-		
 		httpServer.start();
+		
+		//create a http server specifically for the chronotimer to connect to.
+		chronoServer = HttpServer.create(new InetSocketAddress(8080), 0);
+		chronoServer.createContext("/display", new ChronoTimerPostHandler());
+		chronoServer.start();
+		
 		isInitialized = true;
 	}
 	
@@ -43,6 +50,41 @@ public class ChronoTimerWebServer {
             os.write(html.getBytes());
             os.close();
         }
+    }
+    
+    static class ChronoTimerPostHandler implements HttpHandler {
+    	public void handle(HttpExchange t) throws IOException {
+    		if (t.getRequestMethod() == "POST" || t.getRequestMethod() == "PUT") {
+    			//handle the post.
+    			Gson g = new Gson();
+    			//grab the response string from the request
+    			String response = readResponseAsString(t);
+    			
+    			//deserialize response (assuming it is json) into an object using the Gson lib.
+    			
+    		} else {
+    			//http status code 501 "not implemented"
+    			//https://docs.oracle.com/javase/8/docs/jre/api/net/httpserver/spec/com/sun/net/httpserver/HttpExchange.html#sendResponseHeaders-int-long-
+    			t.sendResponseHeaders(501, -1);
+    		}
+    		t.close();
+    	}
+    	
+    	private String readResponseAsString(HttpExchange transmission) throws IOException {
+    		// set up a stream to read the body of the request
+            InputStream inputStr = transmission.getRequestBody();
 
+            // string to hold the result of reading in the request
+            StringBuilder sb = new StringBuilder();
+
+            // read the characters from the request byte by byte
+            int nextChar = inputStr.read();
+            while (nextChar > -1) {
+                sb=sb.append((char)nextChar);
+                nextChar=inputStr.read();
+            }
+           
+            return sb.toString();
+    	}
     }
 }
